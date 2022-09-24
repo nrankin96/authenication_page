bbbb62d6c5135ed1fd318cd59ff8efd6dd7eea35
nrankin96 <nykiarankin96@gmail.com>
2022-09-23 21:23:23 -0400

    inital commit

---
+++ app.js
@@ -0,0 +1,22 @@
+//jshint esversion:6
+const express = require('express');
+const bodyParser = require('body-parser');
+const eje = require('ejs');
+
+const app = express();
+
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+
+
+
+
+
+app.listen(3000, () => {
+    console.log('Server started on port 3000.');
+    
+})
