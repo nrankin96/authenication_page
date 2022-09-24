1e3958cf985f8fa3d59d95f4533469ace8c986fe
nrankin96 <nykiarankin96@gmail.com>
2022-09-23 21:40:50 -0400

    update register page

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const eje = require('ejs');
-
+const mongoose = require('mongoose');
 const app = express();
 
 app.use(express.static('public'));
@@ -12,8 +12,40 @@ app.use(bodyParser.urlencoded({
 }));
 
 
+mongoose.connect('mongodb://localhost:27017/userDB', {useNewUrlParser: true});
 
+const userSchema = {
+    email: String,
+    password: String
+};
 
+const User = new mongoose.model('User', userSchema)
+
+app.get('/', (req, res) => {
+    res.render('home');
+});
+
+app.get('/login', (req, res) => {
+    res.render('login');
+});
+
+app.get('/register', (req, res) => {
+    res.render('register');
+});
+
+app.post('/register', (req, res) => {
+    const newUser = new User({
+        email: req.body.username,
+        password: req.body.password
+    });
+    newUser.save((err) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.render('secrets')
+        }
+    })
+})
 
 
 app.listen(3000, () => {
