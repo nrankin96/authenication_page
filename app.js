d5705dad02cbafca7b203374e60b41b7b70d18eb
nrankin96 <nykiarankin96@gmail.com>
2022-09-25 17:56:35 -0400

    add environment vars

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 //jshint esversion:6
+require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const eje = require('ejs');
 const mongoose = require('mongoose');
 const app = express();
 const encrypt = require('mongoose-encryption')
+
+
 
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
@@ -20,9 +23,7 @@ const userSchema = new mongoose.Schema({
     password: String
 });
 
-const secret = 'Thisisourlittlesecret';
-
-userSchema.plugin(encrypt, { secret: secret, encryptedFields: ['password'] });
+userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ['password'] });
 
 const User = new mongoose.model('User', userSchema)
 
